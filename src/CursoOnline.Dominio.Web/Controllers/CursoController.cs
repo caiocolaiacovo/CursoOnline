6e53bad50cff14c66d78804b47d46a38dc7f1aeb
Caio Colaiacovo Carneiro da Costa <caiohshot@gmail.com>
2018-08-10 22:13:40 -0400

    Adicionando projeto web

---
+++ src/CursoOnline.Dominio.Web/Controllers/CursoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CursoOnline.Dominio.Web.Models;
+
+namespace CursoOnline.Dominio.Web.Controllers
+{
+    public class CursoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Novo()
+        {
+            return View();
+        }
+
+        public IActionResult Salvar()
+        {
+            return View();
+        }
+    }
+}
